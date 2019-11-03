7e1ec8c9a37bf9a1ead2440a2a5fa93d989ecdd2
sumairlaghari <57317002+sumairlaghari@users.noreply.github.com>
2019-11-03 16:50:01 +0500

    Assignments by Sumair

--- As4.py
+++ As4.py
@@ -1,9 +1,5 @@
 #SMIT/19/PY/B2/08727 PY08727 SUMAIR AIJAZ aliasadzaidi123@gmail.com
 
-#from math import pi
-#r = float(input ("Input the radius of the circle : "))
-#print ("The area of the circle with radius " + str(r) + " is: " + str(pi * r**2))
-
 pi=3.14
 r = float(input ("Input the radius of the circle: "))
 area = pi*(r**2)
